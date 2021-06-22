273f5dddf25cd37c11180bec38bcc435d3f03bc7
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-06-22 21:18:15 +0530

    30.building a widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new MyApp());
-}
+// void main() {
+//   runApp(new MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(home: Text('Hello!'));
+    return new MaterialApp(
+      home:Scaffold(
+        appBar:new AppBar(
+          title: new Text('My first app'),),
+        body: Text('This is my default text'),
+        ),
+      );
   }
 }
