e4ad883696ba9e857d102ff741ceef1f2a4def17
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-06-25 09:02:25 +0530

    39.using private properties-method

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
     _questionIndex = _questionIndex + 1;
     });
@@ -39,7 +39,7 @@ class _MyAppState extends State<MyApp> {
           Text(questions[_questionIndex]),
           RaisedButton(
             child: Text('Answer 1'),
-            onPressed: answerQuestion,
+            onPressed: _answerQuestion,
           ),
           RaisedButton(
             child: Text('Answer 2'),
