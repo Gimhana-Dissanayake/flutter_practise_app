8020b06b8c1734ac80637e56b32643592ce76759
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-06-25 09:01:42 +0530

    39.using private properties

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,18 @@ void main() => runApp(MyApp());
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return MyAppState();
+    return _MyAppState();
   }
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
   void answerQuestion() {
     setState(() {
-      questionIndex = questionIndex + 1;
+    _questionIndex = _questionIndex + 1;
     });
-    print(questionIndex);
+    print(_questionIndex);
   }
 
   @override
@@ -36,7 +36,7 @@ class MyAppState extends State<MyApp> {
           title: new Text('My first app'),
         ),
         body: Column(children: <Widget>[
-          Text(questions[questionIndex]),
+          Text(questions[_questionIndex]),
           RaisedButton(
             child: Text('Answer 1'),
             onPressed: answerQuestion,
