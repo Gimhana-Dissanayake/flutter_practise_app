4abe94b47fb335610dd36f47166db26902ec3228
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-07-11 10:37:41 +0530

    53.getters & else if

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -7,7 +7,7 @@ class Quiz extends StatelessWidget {
 
   final List<Map<String, Object>> questions;
   final int questionIndex;
-  final VoidCallback answerQuestion;
+  final Function answerQuestion;
 
   Quiz({required this.questions,required this.answerQuestion,required this.questionIndex});
 
@@ -16,8 +16,8 @@ class Quiz extends StatelessWidget {
     return Column(
       children: <Widget>[
         Question(questions[questionIndex]['questionText'] as String),
-        ...(questions[questionIndex]["answers"] as List<String>).map((answer) {
-          return new Answer(answerQuestion, answer);
+        ...(questions[questionIndex]["answers"] as List<Map<String,Object>>).map((answer) {
+          return new Answer(() => answerQuestion(answer['score']), answer['text'] as String);
         }).toList()
       ],
     );
